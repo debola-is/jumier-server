3c04e9f3661988ae1a3ec16f4c8f25566ef9a1d5
debola-is <adebolasanni079@gmail.com>
2022-11-22 09:29:26 +0100

    created user model and schema

---
+++ src/models/model_user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { productSchema } = require('./model_product');
+
+// Schema is basically the structure of our user
+const userSchema = mongoose.Schema({
+    name: {
+        required: true,
+        type: String,
+        trim: true //remove leading and trailing whitespaces
+    },
+    email: {
+        required: true,
+        type: String,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        default: "",
+    },
+    userType: {
+        type: String,
+        default: "user",
+    },
+    cart: [
+        {
+           product: productSchema, 
+           quantity: {
+            type: Number,
+            required: true,
+           }
+        },
+    ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
